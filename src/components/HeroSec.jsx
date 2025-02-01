24805ffbac8f6fb1c7cf6edc5670e14d73499f64
Edward <kemuthii@gmail.com>
2025-02-01 12:10:52 +0300

    added image folder on asset folder to hold my images,temoved styling from app.css and added them to index.css,modified HeroSec.jsx and added its stylling on HeroSecStyles.css.

--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -1,3 +1,4 @@
+
 import './HerosecStyles.css'
 
 
@@ -5,7 +6,14 @@ import './HerosecStyles.css'
 export default function Footer(){
     return(
         <section>
-            <p>hey there</p>
+            
+            <div className='description'>
+                <h3>you got the travel plans we got the travel vans</h3>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+            </div>
+            <div className='btn'>
+                <button>Find your van</button>
+            </div>
         </section>
     )
 }
