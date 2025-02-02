4f0f1a6ad946cb134cdaa99e7e00d27c842e1709
Edward <kemuthii@gmail.com>
2025-02-02 08:18:24 +0300

    added a Vans.jsx page,added Van.js component and creted its compliment style file vanStyles.css

---
+++ src/components/van.jsx
@@ -0,0 +1,22 @@
+import './VanStyles.css'
+import { Link} from "react-router-dom"
+import { FaBars, FaTimes } from "react-icons/fa"
+import React from 'react'
+
+
+export default function Footer(){
+    const [isOpen,setIsOpen] =React.useState(false)
+    return(
+        <header>
+            <h1>#vanlife</h1>
+            <ul className={`listitems ${isOpen ? "active" : ""}`}>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to = "about/">About</Link></li>
+                <li><Link to = "vans/">Vans</Link></li>
+            </ul>
+            <div className="hamburger" onClick={()=>setIsOpen(!isOpen)}>
+                {isOpen?<FaTimes size={20}/>: <FaBars size={20} />}
+            </div>
+        </header>
+    )
+}
---
+++ src/pages/Vans.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../components/Navbar.jsx'
+import Van from '../components/Van.jsx'
+import Footer from '../components/Footer.jsx'
+
+
+
+export default function Home(){
+    return(
+        <div>
+            <Navbar />
+            <Van />
+            <Footer />
+        </div>
+    )
+}
