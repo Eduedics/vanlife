34769cd42427ea8602e5a210f2480d774e508f31
Edward <kemuthii@gmail.com>
2025-02-03 11:24:29 +0300

    fetched data from API and displayed it to the page

--- src/components/Van.jsx
+++ src/components/Van.jsx
@@ -1,13 +1,37 @@
 import './VanStyles.css'
 // import { Link} from "react-router-dom"
 // import { FaBars, FaTimes } from "react-icons/fa"
-// import React from 'react'
+import React from 'react'
 
 
 export default function Footer(){
+    const [vanData,setVanData] =React.useState([])
+    React.useEffect(
+        ()=>{
+            fetch("")
+                .then(res=>res.json())
+                .then(data=>setVanData(data.vans))
+        },[]
+    )
     return(
         <div>
-            hey there
+            <h1>Exprole van options</h1>
+            {vanData.map( myData=>(
+                <div key={myData.id}>
+                    <figure>
+                        <img src={myData.imgUrl} alt='van img'/>
+                        <figcaption>van img</figcaption>
+                    </figure>
+                    <h3>{myData.name}</h3>
+                    <p>{myData.Price}</p>
+                    <div>
+                        <i>{myData.type}</i>
+                    </div>
+                </div>
+            ))
+            }
         </div>
-    )
+        
+        )
+        
 }
