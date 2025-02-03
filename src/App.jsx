4af8fb50ddc95169cd308029635591b9379a90af
Edward <kemuthii@gmail.com>
2025-02-03 11:37:21 +0300

    added detail page for vans based on id,added equivalent component VanDetails.jsx which will contain details for every van,added VanDetailsStyles.css to hold the styles for detail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 import Vans from "./pages/Vans.jsx"
+import VansDetails from "./pages/VansDetails.jsx"
 import './App.css'
 
 import { Route, Routes} from "react-router-dom"
@@ -11,6 +12,7 @@ function App() {
         <Route path="" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/vans" element={<Vans/>}/>
+        <Route path="/vans/:id" element={<VansDetails/>}/>
       </Routes>
 
 
---
+++ src/pages/VansDetails.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../components/Navbar.jsx'
+import VanDetails from '../components/VanDetails.jsx'
+import Footer from '../components/Footer.jsx'
+
+
+
+export default function Home(){
+    return(
+        <div>
+            <Navbar />
+            <VanDetails />
+            <Footer />
+        </div>
+    )
+}
